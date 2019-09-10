25659952398d7a0e3113492e5a9a1040786fd2dd
Domenico <domedfd@gmail.com>
2019-09-10 15:57:07 -0400

    bung y iniciar monitor automatico en pm2

--- src/config/cors.js
+++ src/config/cors.js
@@ -6,7 +6,7 @@ module.exports = function(req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Contet-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 };
